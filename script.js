ae0cb9b40fe791d6295695855ce581aef64f969e
Bryan Thompson <114091741+bthompson1223@users.noreply.github.com>
2023-06-22 14:07:11 -0500

    Changed timing of mole moving to make game slightly more easy.

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-const squares = document.querySelectorAll('.square')
-const mole = document.querySelector('.mole')
-const timeLeft = document.querySelector('#time-left')
-const score = document.querySelector('#score')
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const timeLeft = document.querySelector("#time-left");
+const score = document.querySelector("#score");
 
-let result = 0
-let hitPosition
-let currentTime = 15
-let countDownTimerId = setInterval(countDown, 1000)
+let result = 0;
+let hitPosition;
+let currentTime = 15;
+let countDownTimerId = setInterval(countDown, 1000);
 let timerId = null;
 
 function randomSquare() {
-    squares.forEach(square => square.classList.remove('mole'));
+  squares.forEach((square) => square.classList.remove("mole"));
 
-    let randomSquare = squares[Math.floor(Math.random() * 9)]
-    randomSquare.classList.add('mole')
+  let randomSquare = squares[Math.floor(Math.random() * 9)];
+  randomSquare.classList.add("mole");
 
-    hitPosition = randomSquare.id
+  hitPosition = randomSquare.id;
 }
 
-squares.forEach(square  => {
-    square.addEventListener('mousedown', () => {
-        if (square.id === hitPosition) {
-            result++;
-            score.textContent = result;
-            hitPosition = null
-        }
-    })
-})
+squares.forEach((square) => {
+  square.addEventListener("click", () => {
+    if (square.id === hitPosition) {
+      result++;
+      score.textContent = result;
+      hitPosition = null;
+    }
+  });
+});
 
 function moveMole() {
-    
-    timerId = setInterval(randomSquare, 500)
+  timerId = setInterval(randomSquare, 600);
 }
 
-moveMole()
+moveMole();
 
 function countDown() {
-    currentTime--;
-    timeLeft.textContent = currentTime
+  currentTime--;
+  timeLeft.textContent = currentTime;
 
-    if (currentTime === 0) {
-        clearInterval(countDownTimerId)
-        clearInterval(timerId)
-        squares.forEach(square => square.removeEventListener('mousedown', () => {
-            if (square.id === hitPosition) {
-                result++;
-                score.textContent = result;
-                hitPosition = null
-            }
-        }))
-        alert(`Game Over! Your final score is ${result}!`)
-    }
+  if (currentTime === 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+    squares.forEach((square) =>
+      square.removeEventListener("mousedown", () => {
+        if (square.id === hitPosition) {
+          result++;
+          score.textContent = result;
+          hitPosition = null;
+        }
+      })
+    );
+    alert(`Game Over! Your final score is ${result}!`);
+  }
 }
-
